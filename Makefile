29cc9924ce2411a753727a469255e55c9cc69193
Patryk Cislo <patrykcislo@gmail.com>
2018-04-12 17:20:17 +0200

    fix configs

--- Makefile
+++
@@ -1,6 +0,0 @@
-CC=$(CROSS_COMPILE)gcc
-OBJS := src/main.o
-combination-lock: $(OBJS)
-	$(CC) -g -o combination-lock $(OBJS) -l wiringPi
-$(OBJS) : %.o : %.c
-	$(CC) -c $(CFLAGS) $< -o $@
--- package/combinationlock.mk
+++ package/combinationlock.mk
@@ -1,12 +1,12 @@
 COMBINATIONLOCK_VERSION = 1.0
-COMBINATIONLOCK = $(TOPDIR)/../../raspi-combination-lock/src
+COMBINATIONLOCK_SITE = $(TOPDIR)/../raspicombinationlock/src
 COMBINATIONLOCK_SITE_METHOD = local
 COMBINATIONLOCK_DEPENDENCIES = wiringpi
 define COMBINATIONLOCK_BUILD_CMDS
-$(MAKE) $(TARGET_CONFIGURE_OPTS) combination-lock -C $(@D)
+$(MAKE) $(TARGET_CONFIGURE_OPTS) combinationlock -C $(@D)
 endef
 define COMBINATIONLOCK_INSTALL_TARGET_CMDS 
-$(INSTALL) -D -m 0755 $(@D)/combination-lock $(TARGET_DIR)/usr/bin
+$(INSTALL) -D -m 0755 $(@D)/combinationlock $(TARGET_DIR)/usr/bin
 endef
 COMBINATIONLOCK_LICENSE = GPL
 $(eval $(generic-package))
